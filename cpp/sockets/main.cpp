c771c54377bea60e6843f0da5c15c1a9ed093e20
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2017-02-21 02:18:19 +0400

    Initial commit

---
+++ cpp/sockets/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello from C++\n";
+    return 0;
+}
---
+++ python/run.py
@@ -0,0 +1,4 @@
+print('hello from python')
+x = 3
+if x > 1:
+    print('ok')
