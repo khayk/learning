ae66f915edb12fc4548b5c64cc9d8abce2502d91
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2018-08-21 19:10:46 +0400

    * Added cmake test script

--- cpp/src/boost/main.cpp
+++ cpp/src/boost/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <boost/make_shared.hpp>
+#include <boost/shared_ptr.hpp>
 
 void fn()
 {
-  std::cout << "fn called\n";
+  boost::shared_ptr<int> sp = boost::make_shared<int>(4);
+  std::cout << "fn called\n" << *sp;
 }
 
 int main(int, char*[])
