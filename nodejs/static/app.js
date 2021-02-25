5ac95c73acd30eb1cdcf034ec61597d4eed4577f
Hayk Karapetyan <hayk.karapetyan@disqo.com>
2021-02-26 01:54:55 +0400

    Second checkpoint

--- nodejs/static/app.js
+++ nodejs/static/app.js
@@ -1,31 +1,31 @@
-const populateProducts = async () => {
-    const products = document.querySelector('#products')
-    products.innerHTML = ''
-    const data = [
-      {id: 'A1', name: 'Vacuum Cleaner', rrp: '99.99', info: 'The suckiest vacuum in the world.'},
-      {id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.'},
-      {id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Delicious overpriced chocolate.'}
-    ]
-    for (const product of data) {
-      const item = document.createElement('product-item')
-      for (const key of ['name', 'rrp', 'info']) {
-        const span = document.createElement('span')
-        span.slot = key
-        span.textContent = product[key]
-        item.appendChild(span)
-      }
-      products.appendChild(item)
+const API = 'http://localhost:3000'
+
+const populateProducts = async (category) => {
+  const products = document.querySelector('#products')
+  products.innerHTML = ''
+  const res = await fetch( ` ${API}/${category} ` )
+  const data = await res.json()
+  for (const product of data) {
+    const item = document.createElement('product-item')
+    for (const key of ['name', 'rrp', 'info']) {
+      const span = document.createElement('span')
+      span.slot = key
+      span.textContent = product[key]
+      item.appendChild(span)
     }
+    products.appendChild(item)
   }
+}
+const category = document.querySelector('#category')
 
-  document.querySelector('#fetch').addEventListener('click', async () => {
-    await populateProducts()
-  })
+category.addEventListener('input', async ({ target }) => {
+  await populateProducts(target.value)
+})
 
-  customElements.define('product-item', class Item extends HTMLElement {
-    constructor() {
-      super()
-      const itemTmpl = document.querySelector('#item').content
-      this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true))
-    }
-  })
+customElements.define('product-item', class Item extends HTMLElement {
+  constructor() {
+    super()
+    const itemTmpl = document.querySelector('#item').content
+    this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true))
+  }
+})
