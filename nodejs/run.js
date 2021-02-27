2699d3f8171d920e3fe6d6e9a4c0aeafdc96f4f4
Hayk Karapetyan <hayk.karapetyan@disqo.com>
2021-02-27 17:04:35 +0400

    Some tests

--- nodejs/run.js
+++ nodejs/run.js
@@ -1,40 +1,23 @@
-console.log("Hello from NodeJS");
+var intervals = 0;
+var timeouts = 0;
+var stopRequested = false;
 
+var idTm = setTimeout(function(){
+    //console.log("Called by timeout");
+    ++timeouts;
+}, 10);
 
-var mem = {};
+var idIv = setInterval(function(){
+    //console.log("Called by interval");
+    ++intervals;
+}, 10);
 
-var yourself = {
-    solution : function(a, n) {
-        var s = new Array(n);
-        var p = new Array(n);
+setTimeout(function() {
+    clearInterval(idIv);
+    clearTimeout(idTm);
 
-        s[0]     = a[0];
-        p[n - 1] = a[n - 1];
+    stopRequested = true;
 
-        for (var i = 1; i < n; ++i) {
-            s[i] = s[i - 1] + a[i];
-            p[n - i - 1] = p[n - i] + a[n - i - 1];
-        }
-
-        for (var i = 1; i < n - 1; ++i) {
-            if (s[i - 1] == p[i + 1]) {
-                console.log(i);
-//                return i;
-            }
-        }
-
-        if (s[n - 2] == 0)
-            console.log(n - 1);
-
-        if (p[1] == 0)
-            console.log(1);
-
-        return -1;
-    }
-};
-
-//var a = [-1, 3, -4, 5, 1, -6, 2, 1];
-var a = [0, -2147483648, -2147483648];
-console.log(yourself.solution(a, a.length));
-
-
+    console.log("Intervals: ", intervals);
+    console.log("Timeouts: ", timeouts);
+}, 1100);
